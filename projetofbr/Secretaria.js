67b9fe03f49f05194477a1ca3ac8b34e507a5d9a
higuin0677-afk <higuin0677@gmail.com>
2026-06-10 09:24:51 -0300

    Add files via upload

---
+++ projetofbr/Secretaria.js
@@ -0,0 +1,11 @@
+class Secretaria {
+    constructor(nome, email, telefone, senha, cpf) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+        this.cpf = cpf;
+    }
+}
+
+module.exports = Secretaria;
---
+++ projetofbr/paciente.js
@@ -0,0 +1,10 @@
+class Paciente {
+    constructor(nome, cpf, dataNascimento, diagnostico, responsavel, telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.diagnostico = diagnostico;
+        this.responsavel = responsavel;
+        this.telefone = telefone;
+    }
+}
