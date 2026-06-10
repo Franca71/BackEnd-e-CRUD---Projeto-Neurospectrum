47079088878f28feb9c43927e296a013754edce3
Higor <higuin0677-afk>
2026-06-10 10:11:05 -0300

    Atualiza sistema com classes e funcionalidades

---
+++ projetofbr/Consulta.js
@@ -0,0 +1,10 @@
+class Consulta {
+    constructor(data, horario, paciente, profissional) {
+        this.data = data;
+        this.horario = horario;
+        this.paciente = paciente;
+        this.profissional = profissional;
+    }
+}
+
+module.exports = Consulta;
---
+++ projetofbr/Receita.js
@@ -0,0 +1,10 @@
+class Receita {
+    constructor(data, paciente, profissional, descricao) {
+        this.data = data;
+        this.paciente = paciente;
+        this.profissional = profissional;
+        this.descricao = descricao;
+    }
+}
+
+module.exports = Receita;
