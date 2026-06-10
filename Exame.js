0c2d7048a476a2588ab66020f07db2be8647f418
DaviLins006 <araujolins3@gmail.com>
2026-06-10 09:48:15 -0300

    Adiciona classes Exame, Receita e Prontuario

---
+++ Exame.js
@@ -0,0 +1,10 @@
+class Exame {
+    constructor(data, paciente, profissional, tipoExame) {
+        this.data = data;
+        this.paciente = paciente;
+        this.profissional = profissional;
+        this.tipoExame = tipoExame;
+    }
+}
+
+module.exports = Exame;
---
+++ Prontuario.js
@@ -0,0 +1,9 @@
+class Prontuario {
+    constructor(data, paciente, observacoes) {
+        this.data = data;
+        this.paciente = paciente;
+        this.observacoes = observacoes;
+    }
+}
+
+module.exports = Prontuario;
